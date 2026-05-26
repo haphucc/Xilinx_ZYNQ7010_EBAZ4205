b1be1e6c486f854e611d8bc43ac1a9a835187130
food <phuchatao2002@gmail.com>
2026-05-26 12:10:59 +0700

    JTAG xilinx - XVC with Pico
    Mạch nạp jtag xilinx với Raspberry Pico

---
+++ Mạch nạp JTAG XVC - Raspberry Pico/xvc-pico-ng/README.md
@@ -0,0 +1,190 @@
+## pico-xvc
+
+This project implements a Raspberry Pico based Xilinx Virtual Cable (XVC). It
+allows using Raspberry Pico as a JTAG adapter for programming Xilinx FPGAs.
+
+It is derived from the excellent [pico-dirtyJtag](https://github.com/phdussud/pico-dirtyJtag/) project.
+
+A special shoutout goes to https://github.com/tom01h for crazily improving the
+performance of this project - thank you!
+
+The pinout is as follows (it maches the pin names of the target device's JTAG port, in other word - you DON'T need to swap TDI and TDO pins like you would do with the interfaces like SPI or UART):
+
+| Pin name | GPIO   |
+|:---------|:-------|
+| TDI      | GPIO16 |
+| TDO      | GPIO17 |
+| TCK      | GPIO18 |
+| TMS      | GPIO19 |
+| GND      | Pin 23 |
+
+Note: The Raspberry Pico is a 3.3v device. Ensure that the target device and
+the Pico are electrically compatible before connecting them.
+
+![Pinout image](./pinout.png)
+
+![Full Pinout](./raspberry-pi-pico-gpio-pinout-diagram.png)
+
+### Compatibility tests
+
+The project has been verified to be working with the following hardware and software combinations:
+
+
+| Programmer Board    | Firmware      | Target                                  | Software                    | Date        |
+|---------------------|---------------|-----------------------------------------|-----------------------------|-------------|
+| Raspberry Pi Pico   | xvc-pico      | EBAZ4205 'Development' FPGA Board       | Vivado 2021.1               | August 2021 |
+| Raspberry Pi Pico   | xvc-pico      | EBAZ4205 'Development' FPGA Board       | Vivado ML Standard 2023.1   | May 2023    |
+| Raspberry Pi Pico W | xvc-pico-wifi | Unknown                                 | Vivado v2023.1 (64-bit)     | April 2024  |
+| Raspberry Pi Pico   | xvc-pico      | Coolrunner II XC2C64A (Matrix Glitcher) | Xilinx ISE 14.7 (Linux x64) | August 2024 |
+
+
+### Building pico-xvc (for Linux users)
+
+Shortcut: Upload the pre-built `xvcPico.uf2` file to the Raspberry Pico
+Board. Done - skip to the next section.
+
+Install dependencies:
+
+```
+sudo apt install cmake gcc-arm-none-eabi libnewlib-arm-none-eabi \
+  libstdc++-arm-none-eabi-newlib git libusb-1.0-0-dev build-essential \
+  make g++ gcc
+```
+
+```
+mkdir ~/repos
+cd ~/repos
+
+git clone https://github.com/raspberrypi/pico-sdk.git
+cd pico-sdk; git submodule update --init
+
+cd ~/repos
+git clone https://github.com/kholia/xvc-pico.git
+```
+
+Build the host-side daemon:
+
+```
+cd ~/repos/xvc-pico/daemon
+cmake .
+make
+sudo ./xvcd-pico  # run on the host computer, turn on the pico board before
+```
+
+Build the Raspberry Pico's firmware:
+
+```
+cd ~/repos/xvc-pico/firmware
+export PICO_SDK_PATH=${HOME}/repos/pico-sdk
+cmake .
+make -j4
+```
+
+### Windows Notes
+
+Grab `xvcd-pico.exe` from the `builds` folder of this repository itself.
+
+You need to install the `libusbK` driver with `Zadig` (https://zadig.akeo.ie/).
+
+OR
+
+You can install the WinLibUSB driver from `USB Drive Tool Application`
+(https://visualgdb.com/UsbDriverTool/).
+
+Credit goes to https://github.com/benitoss/ for these instructions.
+
+
+### Usage
+
+On the host computer with the Raspberry Pi Pico connected, run the XVC Daemon
+Server.
+
+```
+./xvcd-pico
+```
+
+In Vivado, select the `Add Xilinx Virtual Cable (XVC)` option in the `Hardware
+Manager` and mention the `IP address` and the `Port` of the host computer.
+
+![Vivado Usage](./Usage-in-Vivado.png)
+
+
+### Tips
+
+If you see the `End of startup status: LOW` error message in Vivado, check the
+FPGA power supply's voltage and current ratings.
+
+I found `Maker Pi Pico Base` really helpful for debugging GPIO stuff.
+
+[This URL](https://github.com/aws/aws-fpga/blob/master/hdk/docs/Virtual_JTAG_XVC.md)
+says that Xilinx Virtual Cable (XVC) protocol allows (local or remote) Vivado
+to connect to a target FPGA for debug leveraging standard Xilinx standard debug
+cores like Integrated Logic Analyzer - ILA, Virtual Input/Output - VIO, and
+others. `ILA` works fine over our Raspberry Pico JTAG adapter. Vitis stuff is
+also supported.
+
+
+### Rough Performance Stats ("Speed")
+
+If cost and ease-of-availability are the driving constraints (at the cost of
+speed), then this project is 'usable' and can probably suffice. If higher
+programming speed is a requirement, I recommend using `xc3sprog` or
+`openFPGALoader` with an FT2232H board.
+
+This project might be one of the cheapest, most energy-efficient `Vivado-Compatible`
+JTAG programmers. I use this project as my "daily driver" now ;)
+
+Writing `ebaz4205_top.bit (371.6 KiB)` takes only ~2.5 seconds now - thanks to
+tom01h. Don't `overestimate` the speed of human programmers and Vivado though
+;)
+
+```
+$ time ./openFPGALoader -c xvc-client --port 2542 --file-type bin ebaz4205_top.bin
+empty
+detected xvcServer version v1.0 packet size 12288
+freq 6000000 166.666667 166 0
+a6 0 0 0
+Open file DONE
+Parse file DONE
+load program
+Load SRAM: [==================================================] 100.00%
+Done
+./openFPGALoader -c xvc-client --port 2542 --file-type bin ebaz4205_top.bin  0.00s user 0.01s system 0% cpu 7.389 total
+```
+
+It seems `openFPGALoader` is a lot slower than Vivado in programming devices
+via `XVC`!?
+
+Writing `corescore_0.bit (2 MiB)` which uses ~100% FPGA LEs takes around ~9
+seconds.
+
+
+### Flash FPGA without Vivado
+
+```
+./openFPGALoader -c xvc-client --port 2542 --file-type bin ebaz4205_top.bin
+```
+
+
+### USB UARTs
+
+Connect Pico's hardware UART pins to FPGA's UART.
+
+```
+#define UART_TX_PIN 0
+#define UART_RX_PIN 1
+```
+
+Set UART of FPGA to 115200 baud rate.
+
+Note: /dev/ttyACM(n) will appear when Pico's USB is connected.
+
+
+### Related Ideas / Projects
+
+- https://github.com/kholia/xvcpi
+- https://github.com/kholia/xvc-esp32
+- https://github.com/kholia/xvc-esp8266
+- https://github.com/phdussud/pico-dirtyJtag/
+- https://github.com/kholia/Colorlight-5A-75B
+- https://github.com/fusesoc/blinky#ebaz4205-development-board
---
+++ Mạch nạp JTAG XVC - Raspberry Pico/xvc-pico-ng/daemon/xvcpico.c
@@ -0,0 +1,493 @@
+/*
+   Description: Xilinx Virtual Cable Server for Raspberry Pico Board.
+
+   Glued together by Dhiru Kholia (VU3CER) in August, 2021.
+
+   Optimizations by https://github.com/tom01h (October 2021).
+
+   Logic: We already have a working `xvcpi` program. We retain this program
+   structure as it is, and replace the 'GPIO functions' with libusb calls. On
+   the Raspberry Pico end, we provide functions to match these libusb calls.
+
+   Upstream sources:
+   - https://github.com/trabucayre/openFPGALoader/blob/master/src/dirtyJtag.cpp (Apache-2.0)
+   - https://github.com/kholia/xvcpi/blob/master/xvcpi.c (CC0 1.0 Universal)
+   - https://github.com/phdussud/pico-dirtyJtag/blob/master/cmd.c
+
+   See Licensing information at End of File.
+*/
+
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <netinet/in.h>
+#include <netinet/tcp.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+
+// #define BUFFER_SIZE 1024 * 1024  // is super fast but doesn't work on ebaz4205 board ;(
+#define BUFFER_SIZE 1024 * 20 // NOTE: Reduce this in case of flashing problems!
+
+#ifdef __CYGWIN__
+#include <libusb-1.0/libusb.h>
+#else
+#include <libusb.h>
+#endif
+#define XVCPICO_VID 0x2E8A
+#define XVCPICO_PID 0x000A
+#define XVCPICO_INTF 3
+#define XVCPICO_READ_EP 0x86
+#define XVCPICO_WRITE_EP 0x05
+libusb_context *usb_ctx;
+libusb_device_handle *dev_handle = NULL;
+
+static char xvcInfo[64];
+
+// Note: Modified!
+enum xvcPicoCmd {
+  CMD_STOP = 0x00,
+  CMD_XFER = 0x03,
+  CMD_WRITE = 0x04,
+};
+
+/*
+  enum libusb_error {
+    LIBUSB_SUCCESS             = 0,
+    LIBUSB_ERROR_IO            = -1,
+    LIBUSB_ERROR_INVALID_PARAM = -2,
+    LIBUSB_ERROR_ACCESS        = -3,
+    LIBUSB_ERROR_NO_DEVICE     = -4,
+    LIBUSB_ERROR_NOT_FOUND     = -5,
+    LIBUSB_ERROR_BUSY          = -6,
+    LIBUSB_ERROR_TIMEOUT       = -7,
+    LIBUSB_ERROR_OVERFLOW      = -8,
+    LIBUSB_ERROR_PIPE          = -9,
+    LIBUSB_ERROR_INTERRUPTED   = -10,
+    LIBUSB_ERROR_NO_MEM        = -11,
+    LIBUSB_ERROR_NOT_SUPPORTED = -12,
+    LIBUSB_ERROR_OTHER         = -99,
+  };
+*/
+
+void gpio_send(_Bool header, uint32_t len, uint32_t n, uint8_t *tms, uint8_t *tdi) {
+  unsigned char tx_buffer[512];
+  int actual_length, ret, header_offset = 0;
+
+  int bytes = (n + 7) / 8;
+
+  if (header) {
+    // Replace these with memcpy
+    tx_buffer[header_offset++] = CMD_XFER;
+    tx_buffer[header_offset++] = (len >> 0) & 0xFF;  // uint32_t to bytes
+    tx_buffer[header_offset++] = (len >> 8) & 0xFF;
+    tx_buffer[header_offset++] = (len >> 16) & 0xFF;
+    tx_buffer[header_offset++] = (len >> 24) & 0xFF;
+  }
+
+  for (int i = 0; i < bytes; i++) {
+    tx_buffer[header_offset++] = tms[i];
+    tx_buffer[header_offset++] = tdi[i];
+  }
+
+  actual_length = 0;
+  ret = libusb_bulk_transfer(dev_handle, XVCPICO_WRITE_EP, tx_buffer, header_offset, &actual_length, 1000);
+  if ((ret < 0) || (actual_length != header_offset)) {
+    printf("gpio_xfer_full: usb bulk write failed!\n");
+    return;
+  }
+}
+
+void gpio_recieve(uint32_t n, uint8_t *tdo) {
+  unsigned char result[256];
+  int actual_length, ret;
+
+  int bytes = (n + 7) / 8;
+
+  do {
+    // Note: For a full-speed device, a bulk packet is limited to 64 bytes!
+    // DirtyJTAG USB -> [32597.543687] usb 3-2.3.1: new full-speed USB device number 81 using xhci_hcd
+    ret = libusb_bulk_transfer(dev_handle, XVCPICO_READ_EP, result, bytes, &actual_length, 2000);
+    if (ret < 0) {
+      printf("gpio_xfer_full: usb bulk read failed!\n");
+      printf("[Total Bytes] %d, [Return Code] %d [Actual Length] %d\n", bytes, ret, actual_length);
+      return;
+    }
+  } while (actual_length == 0);
+
+  for (int i = 0; i < bytes; i++) {
+    tdo[i] = result[i];
+  }
+  return;
+}
+
+int device_init() {
+  int ret;
+  struct libusb_device **devs;
+  struct libusb_device *found = NULL;
+  struct libusb_device *dev;
+  size_t i = 0;
+  int r;
+
+  if (libusb_init(&usb_ctx) < 0) {
+    printf("[ERROR] libusb init failed!\n");
+    return -1;
+  }
+
+  // Copy of libusb_open_device_with_vid_pid() code
+  if (libusb_get_device_list(usb_ctx, &devs) < 0)
+    return -1; // XXX
+
+  while ((dev = devs[i++]) != NULL) {
+    struct libusb_device_descriptor desc;
+    r = libusb_get_device_descriptor(dev, &desc);
+    if (r < 0)
+      goto out;
+    if (desc.idVendor == XVCPICO_VID && desc.idProduct == XVCPICO_PID) {
+      found = dev;
+      break;
+    }
+  }
+
+  if (found) {
+    r = libusb_open(found, &dev_handle);
+    if (r < 0) {
+      printf("[ERROR in libusb_open()] %s\n", libusb_error_name(r));
+      dev_handle = NULL;
+    }
+  }
+
+out:
+  libusb_free_device_list(devs, 1);
+
+  if (!dev_handle) {
+    printf("[ERROR] failed to open usb device!\n");
+    libusb_exit(usb_ctx);
+    return -1;
+  }
+  ret = libusb_claim_interface(dev_handle, XVCPICO_INTF);
+  if (ret) {
+    printf("[ERROR in libusb_claim_interface()] %s\n", libusb_error_name(ret));
+    printf("[!] libusb error while claiming XvcPico interface\n");
+    libusb_close(dev_handle);
+    libusb_exit(usb_ctx);
+    return -1;
+  }
+
+  dev = libusb_get_device(dev_handle);
+  int size;
+  size = libusb_get_max_iso_packet_size(dev, XVCPICO_WRITE_EP);
+
+  // printf("write ep size = %d\n", size);
+
+  return size;  // success
+}
+
+void device_close() {
+  if (dev_handle)
+    libusb_close(dev_handle);
+  if (usb_ctx)
+    libusb_exit(usb_ctx);
+}
+
+// This functions handles the following two functions calls:
+// - gpio_write(0, 1, 1);
+// - gpio_write(0, 1, 0);
+// Command Code -> CMD_WRITE
+int gpio_write(int tck, int tms, int tdi) {
+  int actual_length;
+  uint8_t buf[8];
+  u_int buffer_idx = 0;
+
+  buf[buffer_idx++] = CMD_WRITE;
+  buf[buffer_idx++] = tck & 1;
+  buf[buffer_idx++] = tms & 1;
+  buf[buffer_idx++] = tdi & 1;
+  buf[buffer_idx++] = CMD_STOP;
+  int ret = libusb_bulk_transfer(dev_handle, XVCPICO_WRITE_EP, buf, buffer_idx, &actual_length, 1000);
+  if (ret < 0) {
+    printf("gpio_write: usb bulk write failed\n");
+    return -EXIT_FAILURE;
+  }
+
+  return 0;
+}
+
+static int verbose = 0;
+
+static int sread(int fd, void *target, int len) {
+  unsigned char *t = target;
+  while (len) {
+    int r = read(fd, t, len);
+    if (r <= 0)
+      return r;
+    t += r;
+    len -= r;
+  }
+  return 1;
+}
+
+static unsigned char buffer[BUFFER_SIZE], result[BUFFER_SIZE / 2];
+
+int handle_data(int fd, int ep_size) {
+  uint32_t len, nr_bytes;
+
+  do {
+    char cmd[16];
+    memset(cmd, 0, 16);
+
+    if (sread(fd, cmd, 2) != 1)
+      return 1;
+
+    if (memcmp(cmd, "ge", 2) == 0) {
+      if (sread(fd, cmd, 6) != 1)
+        return 1;
+      memcpy(result, xvcInfo, strlen(xvcInfo));
+      if (write(fd, result, strlen(xvcInfo)) != (ssize_t)strlen(xvcInfo)) {
+        perror("write 1");
+        return 1;
+      }
+      if (verbose) {
+        printf("%u : Received command: 'getinfo'\n", (int)time(NULL));
+        printf("\t Replied with %s\n", xvcInfo);
+      }
+      break;
+    } else if (memcmp(cmd, "se", 2) == 0) {
+      if (sread(fd, cmd, 9) != 1)
+        return 1;
+      memcpy(result, cmd + 5, 4);
+      if (write(fd, result, 4) != 4) {
+        perror("write 2");
+        return 2;
+      }
+      if (verbose) {
+        printf("%u : Received command: 'settck'\n", (int)time(NULL));
+        printf("\t Replied with '%.*s'\n\n", 4, cmd + 5);
+      }
+      break;
+    } else if (memcmp(cmd, "de", 2) == 0) {  // DEBUG CODE
+      if (sread(fd, cmd, 3) != 1)
+        return 1;
+      printf("%u : Received command: 'debug'\n", (int)time(NULL));
+      gpio_write(1, 1, 1);
+      break;
+    } else if (memcmp(cmd, "of", 2) == 0) {  // DEBUG CODE
+      if (sread(fd, cmd, 1) != 1)
+        return 1;
+      printf("%u : Received command: 'off'\n", (int)time(NULL));
+      gpio_write(0, 0, 0);
+      break;
+    } else if (memcmp(cmd, "sh", 2) == 0) {
+      if (sread(fd, cmd, 4) != 1)
+        return 1;
+      if (verbose) {
+        printf("%u : Received command: 'shift'\n", (int)time(NULL));
+      }
+    } else {
+      fprintf(stderr, "invalid cmd '%s'\n", cmd);
+      return 1;
+    }
+
+    // Handling for -> "shift:<num bits><tms vector><tdi vector>"
+    if (sread(fd, &len, 4) != 1) {
+      fprintf(stderr, "reading length failed\n");
+      return 1;
+    }
+
+    nr_bytes = (len + 7) / 8;
+    if (nr_bytes * 2 > sizeof(buffer)) {
+      fprintf(stderr, "buffer size exceeded\n");
+      return 1;
+    }
+
+    if (sread(fd, buffer, nr_bytes * 2) != 1) {
+      fprintf(stderr, "reading data failed\n");
+      return 1;
+    }
+    memset(result, 0, nr_bytes);
+
+    if (verbose) {
+      printf("\tNumber of Bits  : %d\n", len);
+      printf("\tNumber of Bytes : %d \n", nr_bytes);
+      printf("\n");
+    }
+
+    // Note
+    gpio_write(0, 1, 1);
+
+    int bytesLeft = nr_bytes;
+    int bitsLeft = len;
+    int byteIndex = 0;
+    int byteIndexr = 0;
+    uint8_t tdi[256], tms[256], tdo[256];
+    _Bool header;
+    int size;
+    int sizer = 0;
+
+    header = 1;
+
+    while (bytesLeft > 0) {
+      tms[0] = 0;
+      tdi[0] = 0;
+      tdo[0] = 0;
+      if (header) {
+        size = ep_size / 2 - 4;
+      } else {
+        size = ep_size / 2;
+      }
+      if (bytesLeft >= size) {
+        memcpy(tms, &buffer[byteIndex], size);
+        memcpy(tdi, &buffer[byteIndex + nr_bytes], size);
+        gpio_send(header, len, size * 8, tms, tdi);
+        if (!header) {
+          gpio_recieve(sizer * 8, tdo);
+          memcpy(&result[byteIndexr], tdo, sizer);
+        }
+        sizer = size;
+        byteIndexr = byteIndex;
+        bytesLeft -= size;
+        bitsLeft -= size * 8;
+        byteIndex += size;
+        header = 0;
+      } else {
+        memcpy(tms, &buffer[byteIndex], bytesLeft);
+        memcpy(tdi, &buffer[byteIndex + nr_bytes], bytesLeft);
+        gpio_send(header, len, bitsLeft, tms, tdi);
+        if (!header) {
+          gpio_recieve(sizer * 8, tdo);
+          memcpy(&result[byteIndexr], tdo, sizer);
+        }
+        gpio_recieve(bitsLeft, tdo);
+        memcpy(&result[byteIndex], tdo, bytesLeft);
+        break;
+      }
+    }
+
+    if (bytesLeft == 0) {
+      gpio_recieve(sizer * 8, tdo);
+      memcpy(&result[byteIndexr], tdo, sizer);
+    }
+
+    gpio_write(0, 1, 0);
+
+    if (write(fd, result, nr_bytes) != nr_bytes) {
+      perror("write 3: Reduce BUFFER_SIZE in xvcpico.c");
+      return 3;
+    }
+
+  } while (1);
+  /* Note: Need to fix JTAG state updates, until then no exit is allowed */
+  return 0;
+}
+
+int main() {
+  int i;
+  int s;
+  struct sockaddr_in address;
+
+  // Init
+  sprintf(xvcInfo, "xvcServer_v1.0:%d\n", BUFFER_SIZE);
+  int ep_size = device_init();
+  if (ep_size < 0) {
+    return -1;
+  }
+  fprintf(stderr, "NB: ep_size => %d\n", ep_size);
+  fprintf(stderr, "XVCPI is listening now with BUFFER_SIZE => %d!\n", BUFFER_SIZE/2);
+
+  s = socket(AF_INET, SOCK_STREAM, 0);
+  if (s < 0) {
+    perror("socket");
+    device_close();
+    return 1;
+  }
+  i = 1;
+  setsockopt(s, SOL_SOCKET, SO_REUSEADDR, &i, sizeof i);
+  address.sin_addr.s_addr = INADDR_ANY;
+  address.sin_port = htons(2542);
+  address.sin_family = AF_INET;
+
+  if (bind(s, (struct sockaddr *)&address, sizeof(address)) < 0) {
+    perror("bind");
+    device_close();
+    return 1;
+  }
+
+  if (listen(s, 0) < 0) {
+    perror("listen");
+    device_close();
+    return 1;
+  }
+
+  fd_set conn;
+  int maxfd = 0;
+  FD_ZERO(&conn);
+  FD_SET(s, &conn);
+  maxfd = s;
+
+  while (1) {
+    fd_set read = conn, except = conn;
+    int fd;
+
+    if (select(maxfd + 1, &read, 0, &except, 0) < 0) {
+      perror("select");
+      break;
+    }
+
+    for (fd = 0; fd <= maxfd; ++fd) {
+      if (FD_ISSET(fd, &read)) {
+        if (fd == s) {
+          int newfd;
+          socklen_t nsize = sizeof(address);
+
+          newfd = accept(s, (struct sockaddr *)&address, &nsize);
+          if (verbose)
+            printf("connection accepted - fd %d\n", newfd);
+          if (newfd < 0) {
+            perror("accept");
+          } else {
+            int flag = 1;
+            int optResult = setsockopt(newfd, IPPROTO_TCP, TCP_NODELAY, (char *)&flag, sizeof(int));
+            if (optResult < 0)
+              perror("TCP_NODELAY error");
+            if (newfd > maxfd) {
+              maxfd = newfd;
+            }
+            FD_SET(newfd, &conn);
+          }
+        } else if (handle_data(fd, ep_size)) {
+          if (verbose)
+            printf("connection closed - fd %d\n", fd);
+          close(fd);
+          FD_CLR(fd, &conn);
+        }
+      } else if (FD_ISSET(fd, &except)) {
+        if (verbose)
+          printf("connection aborted - fd %d\n", fd);
+        close(fd);
+        FD_CLR(fd, &conn);
+        if (fd == s)
+          break;
+      }
+    }
+  }
+
+  device_close();
+  return 0;
+}
+
+/*
+   This work, "xvcpi.c", is a derivative of "xvcServer.c" (https://github.com/Xilinx/XilinxVirtualCable)
+   by Avnet and is used by Xilinx for XAPP1251.
+
+   "xvcServer.c" is licensed under CC0 1.0 Universal (http://creativecommons.org/publicdomain/zero/1.0/)
+   by Avnet and is used by Xilinx for XAPP1251.
+
+   "xvcServer.c", is a derivative of "xvcd.c" (https://github.com/tmbinc/xvcd)
+   by tmbinc, used under CC0 1.0 Universal (http://creativecommons.org/publicdomain/zero/1.0/).
+
+   Portions of "xvcpi.c" are derived from OpenOCD (http://openocd.org)
+
+   "xvcpi.c" is licensed under CC0 1.0 Universal (http://creativecommons.org/publicdomain/zero/1.0/)
+   by Derek Mulcahy.
+*/
